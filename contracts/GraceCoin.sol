8f7ac00d5a5b45d04202a1b1abd2a4887e5a6f01
Thomas S. Bamford <67169135+thoms994@users.noreply.github.com>
2021-12-24 00:00:27 +0100

    master

--- contracts/GraceCoin.sol
+++ contracts/GraceCoin.sol
@@ -1,18 +1,4 @@
-/**
- * author: 0f0crypto <00ff00crypto@gmail.com>
- * discord: https://discord.gg/zn86MDCQcM
- *
- * Gracetoken v1.0beta
- *
- * This is a rewrite of Safemoon in the hope to:
- *
- * - make it easier to change the tokenomics
- * - make it easier to maintain the code and develop it further
- * - remove redundant code
- * - fix some of the issues reported in the Safemoon audit (e.g. SSL-03)
- *      https://www.certik.org/projects/safemoon
- *
- *
+
            _____                    _____                    _____                    _____                    _____          
          /\    \                  /\    \                  /\    \                  /\    \                  /\    \         
         /::\    \                /::\    \                /::\    \                /::\    \                /::\    \        
@@ -54,35 +40,6 @@ import "./Grace-imports.sol";
  * Cachback         2%
  */
 
-
-/**
- * @dev If I did a good job you should not need to change anything apart from the values in the `Tokenomics`,
- * the actual name of the contract `SafeTokenV1Beta` at the very bottom **and** the `environment` into which
- * you are deploying the contract `SafeToken(Env.Testnet)` or `SafeToken(Env.MainnetV2)` etc.
- * 
- * If you wish to disable a particular tax/fee just set it to zero (or comment it out/remove it).
- * 
- * You can add (in theory) as many custom taxes/fees with dedicated wallet addresses if you want. 
- * Nevertheless, I do not recommend using more than a few as the contract has not been tested 
- * for more than the original number of taxes/fees, which is 6 (liquidity, redistribution, burn, 
- * marketing, charity & tip to the dev). Furthermore, exchanges may impose a limit on the total
- * transaction fee (so that, for example, you cannot claim 100%). Usually this is done by limiting the 
- * max value of slippage, for example, PancakeSwap max slippage is 49.9% and the fees total of more than
- * 35% will most likely fail there.
- * 
- * NOTE: You shouldn't really remove the Rfi fee. If you do not wish to use RFI for your token, 
- * you shouldn't be using this contract at all (you're just wasting gas if you do).
- *
- * NOTE: ignore the note below (anti-whale mech is not implemented yet)
- * If you wish to modify the anti-whale mech (progressive taxation) it will require a bit of coding. 
- * I tried to make the integration as simple as possible via the `Antiwhale` contract, so the devs 
- * know exactly where to look and what/how to make the necessary changes. There are many possibilites,
- * such as modifying the fees based on the tx amount (as % of TOTAL_SUPPLY), or sender's wallet balance 
- * (as % of TOTAL_SUPPLY), including (but not limited to):
- * - progressive taxation by tax brackets (e.g <1%, 1-2%, 2-5%, 5-10%)
- * - progressive taxation by the % over a threshold (e.g. 1%)
- * - extra fee (e.g. double) over a threshold 
- */
 abstract contract Tokenomics {
     
     using SafeMath for uint256;
